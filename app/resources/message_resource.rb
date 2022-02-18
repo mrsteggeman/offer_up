6edeb62beff5e7eb49cc80335d07ed14f728a6ef
anonymous anonymous <matt.steggeman@kellogg.northwestern.edu>
2022-02-18 19:41:34 +0000

    Generate graphiti resources for messages

---
+++ app/resources/message_resource.rb
@@ -0,0 +1,13 @@
+class MessageResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :content, :string
+  attribute :item_details_id, :integer
+  attribute :sender_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
