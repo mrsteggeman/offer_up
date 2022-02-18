b03f63de956afd93f25d077c6c2e51ace032f3b1
anonymous anonymous <matt.steggeman@kellogg.northwestern.edu>
2022-02-18 19:40:28 +0000

    Whitelist columns in ItemList for ActiveAdmin

--- app/admin/item_lists.rb
+++ app/admin/item_lists.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ItemList do
 
+ permit_params :item_details_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
