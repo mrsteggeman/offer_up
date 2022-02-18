97382805ac162751c1ec4d4a97f6e290bfc2d8c5
anonymous anonymous <matt.steggeman@kellogg.northwestern.edu>
2022-02-18 19:41:26 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,16 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :price, :string
+  attribute :category_id, :integer
+  attribute :photo, :string
+  attribute :user_id, :integer
+  attribute :comment_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
